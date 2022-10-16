da003f5a756aa0dfaa417426ae3f20698b19a407
Ronzzzzz821 <109229498+Ronzzzzz821@users.noreply.github.com>
2022-10-16 20:42:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285806008040']
+global.ownernomer = "6285806008040"
+global.premium = ['6285806008040']
+global.packname = 'Roni'
+global.author = '<3'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
